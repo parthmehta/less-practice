3e9712bb6a4c79a5b863268163fb38139ad36ef9
Parth <parth.rmehta@gmail.com>
2017-03-26 00:07:59 +0530

    Variables,Extends, Mixins

---
+++ app.js
@@ -0,0 +1,27 @@
+var fs   = require('fs'),
+    path = require('path'),
+    less = require('less'),
+    from = "./style.less";
+
+
+// HELPERS
+function compileLESS (from, to) {
+    from = path.join(__dirname,from);
+    // to = path.join(__dirname,to);
+    fs.readFile(from,function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }else{
+        less.render(data.toString(),{compress:false,paths:[__dirname]},function(e,css){
+            if (!e){
+              console.log(css.css)
+              // fs.writeFile(to,css);
+            }
+        });
+      }
+    });
+
+}
+
+compileLESS(from,null)
